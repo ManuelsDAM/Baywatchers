70537537fd0da64ce5ae5fc8ad8e4b0e17823dae
BaywatchZEfron <alandor324@gmail.com>
2025-05-30 22:25:48 +0100

    cambio a poetry y +

--- baywatchers.py
+++ baywatchers.py
@@ -1,28 +1,9 @@
 from app.bot.telegrambot import main
-from app.bot.notify import send_message
-from decouple import config
 from app.db import engine
 from app.db.models import Base
-from app.scheduler import start_scheduler
-#import threading 
 
+# Crea las tablas si no existen
 Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
-    chat_id = int(config("CHAT_ID"))
-    send_message(chat_id,"🔔 Prueba de notificación automática.")
-    #bot_thread= threading.Thread(target=main)
-    #bot_thread.start()
-    start_scheduler()
     main()
-
-"""import request as req
-
-from telegram.ext import Updater
-from config.auth import token
-if __name__ == '__main__':
-    updater = Updater(token=token)
-    dispatcher = updater.dispatcher
-    
-Url = 'https://api.telegram.org/bot'
-Key = ''"""
